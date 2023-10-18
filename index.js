585e5331ba965d43a603f0e3e4b661c5f4637ea8
Darkspectra <ftanvirnovo@gmail.com>
2023-10-18 20:15:44 +0600

    resolve server error and added DELETE function

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ async function run() {
       res.send(result);
     })
 
+
+
     // Cart API
     app.post("/cart", async (req, res) => {
       const newProduct = req.body;
@@ -56,6 +58,21 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.get("/cart/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.delete("/cart/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result);
+    })
+
 
 
 
